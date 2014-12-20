69dc05c08f7b815234ea1727935cc71706a75579
Fidel Gustavo Aguilar Flores <fgaguilar@yahoo.com>
2014-12-20 10:40:27 -0400

    Adecuaciones para formato de numeros

--- src/js/controllers/form.js
+++ src/js/controllers/form.js
@@ -22,7 +22,6 @@ app.factory('Plan', ['$resource',
       'delete': {method:'DELETE'}
     });
   }]);*/
-/************************************/
 app.run(function($locale){
     $locale.NUMBER_FORMATS.GROUP_SEP = ",";
 });
@@ -61,40 +60,42 @@ app.directive('numericInput', function($filter, $browser, $locale) {
         }        
     }
 })
-/***********************************/
 app.controller('ListCtrl', function($scope, Plan) {
    $scope.planilla = Plan.query();
   // $scope.orderProp = 'id';
 });
 
-/*app.controller('CreateCtrl', function($scope,$location,$timeout, Plan) {
+app.controller('CreateCtrl', function($scope,$location,$timeout, Plan) {
+
     $scope.grabar = function() {
-      console.log("INGRESO A FormDemoCtrl funcion grabar");
-      debugger;
+
       Plan.save($scope.plans, function() {
         $timeout(function() {
           $location.path('/');
         });
       });
     };
-});*/
+  
+});
 
 
 
   // Form controller
 app.controller('FormDemoCtrl',function ($scope,$location,$timeout,Plan) {
-  console.log("INGRESO A FormDemoCtrl");
+    console.groupCollapsed("CreateCtrl");
 
     $scope.planilla={};
 
     $scope.grabar = function() {
-      console.log("INGRESO A FormDemoCtrl funcion grabar");
+      console.error("Ingreso agrabar");
+      debugger;
       Plan.save($scope.planilla, function() {
         $timeout(function() {
           $location.path('/');
         });
       });
     };
+    console.groupEnd();
 
     $scope.planilla.pesoMermaPesos=1.00;
     $scope.planilla.pesoKilosNetosHumedosFactores=2.2046223;
@@ -103,6 +104,10 @@ app.controller('FormDemoCtrl',function ($scope,$location,$timeout,Plan) {
     $scope.planilla.pesoMermaFactores=6.96;
     $scope.planilla.contenidoZnTipoDeCambioFactores=6.96;
     $scope.calcular = function(){
+      $scope.planilla.pesoHumedadPesos=9.18;
+      $scope.planilla.pesoKilosNetosHumedosPeso=1445000.00;
+
+
       $scope.planilla.pesoHumedadPeso=($scope.planilla.pesoHumedadPesos*$scope.planilla.pesoKilosNetosHumedosPeso)/100;
       $scope.planilla.pesoMermaPeso=(($scope.planilla.pesoKilosNetosHumedosPeso-$scope.planilla.pesoHumedadPeso)*$scope.planilla.pesoMermaPesos)/100;
       $scope.planilla.pesoKilosNetosSecosPeso=$scope.planilla.pesoKilosNetosHumedosPeso-$scope.planilla.pesoHumedadPeso-$scope.planilla.pesoMermaPeso;
