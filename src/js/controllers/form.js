e3bb2f22a5061a2700c8c8170562db0389c97de7
Fidel Gustavo Aguilar Flores <fgaguilar@yahoo.com>
2014-12-23 21:09:09 -0700

    Actualizacion de plan de Calculo

--- src/js/controllers/form.js
+++ src/js/controllers/form.js
@@ -53,28 +53,19 @@ app.controller('ListCtrl', function($scope, Plan) {
    $scope.planilla = Plan.query();
   // $scope.orderProp = 'id';
 });
-
 app.controller('CreateCtrl', function($scope,$location,$timeout, Plan) {
-
     $scope.grabar = function() {
-
       Plan.save($scope.plans, function() {
         $timeout(function() {
           $location.path('/');
         });
       });
     };
-  
 });
-
-
-
   // Form controller
 app.controller('FormDemoCtrl',function ($scope,$location,$timeout,Plan) {
     console.groupCollapsed("CreateCtrl");
-
     $scope.planilla={};
-
     $scope.grabar = function() {
       console.error("Ingreso agrabar");
       Plan.save($scope.planilla, function() {
@@ -126,6 +117,67 @@ app.controller('PlanCalculoCtrl',function ($scope,$location,$timeout,$stateParam
   $scope.planilla2={};
   $scope.planilla2=Plan.get({'planillaId': planillaId}, function(datos){
     console.log($scope);
-    $scope.planillaC.d4=datos.baseAgCotizaciones;
+    $scope.planillaC.v7=datos.contenidoZnLeyes;
+    $scope.planillaC.d4=$scope.planillaC.v7;
+    $scope.planillaC.v6=datos.pesoMermaFactores;
+    $scope.planillaC.c7=datos.pesoKilosNetosHumedosPeso;
+    $scope.planillaC.v9=datos.pesoHumedadPesos;
+    $scope.planillaC.x6=$scope.planillaC.c7*$scope.planillaC.v9;
+    $scope.planillaC.v10=datos.pesoMermaPesos;
+    $scope.planillaC.v11=$scope.planillaC.v9+$scope.planillaC.v10;
+    $scope.planillaC.z6=$scope.planillaC.c7*$scope.planillaC.v11;
+    $scope.planillaC.d7=8.57;
+    $scope.planillaC.x7=($scope.planillaC.c7-$scope.planillaC.x6)*$scope.planillaC.v10;
+    $scope.planillaC.c8=$scope.planillaC.c7*$scope.planillaC.v9;
+    $scope.planillaC.c9=($scope.planillaC.c7-$scope.planillaC.c8)*$scope.planillaC.v10;
+    $scope.planillaC.z7=$scope.planillaC.c8+$scope.planillaC.c9;
+    $scope.planillaC.v8=datos.contenidoAgLeyes;
+    $scope.planillaC.x8=$scope.planillaC.x6+$scope.planillaC.x7;
+    $scope.planillaC.z8=$scope.planillaC.z6-$scope.planillaC.z7;
+    $scope.planillaC.c10=$scope.planillaC.c8+$scope.planillaC.c9;
+    $scope.planillaC.c11=$scope.planillaC.c7-$scope.planillaC.c10;
+    $scope.planillaC.c12=$scope.planillaC.d4*100;
+    $scope.planillaC.c13=$scope.planillaC.c11*$scope.planillaC.c12/100;
+    $scope.planillaC.c14=$scope.planillaC.c13*datos.contenidoAgLeyes;
+    $scope.planillaC.d14=58.70;
+    $scope.planillaC.v14=$scope.planillaC.c13;
+    $scope.planillaC.v16=$scope.planillaC.v14+$scope.planillaC.v15;
+    $scope.planillaC.d23=0.98;
+    $scope.planillaC.c21=$scope.planillaC.c11;
+    $scope.planillaC.d20=$scope.planillaC.v8;
+    $scope.planillaC.c22=$scope.planillaC.c21*$scope.planillaC.d20/1000;
+    $scope.planillaC.c23=$scope.planillaC.c22/1000;
+    $scope.planillaC.v15=$scope.planillaC.c23;
+    $scope.planillaC.w14=$scope.planillaC.v14/$scope.planillaC.v16;
+    $scope.planillaC.x14=$scope.planillaC.c7*$scope.planillaC.w14;
+    $scope.planillaC.y14=$scope.planillaC.x8*$scope.planillaC.w14;
+    $scope.planillaC.z14=$scope.planillaC.x14-$scope.planillaC.y14;
+    $scope.planillaC.c15=$scope.planillaC.c13/100;
+    $scope.planillaC.w15=$scope.planillaC.v15/$scope.planillaC.v16;   
+    $scope.planillaC.x15=$scope.planillaC.c7*$scope.planillaC.w15;
+    $scope.planillaC.y15=$scope.planillaC.x8*$scope.planillaC.w15;
+    $scope.planillaC.z15=$scope.planillaC.x15-$scope.planillaC.y15;
+    $scope.planillaC.d16=datos.baseZnCotizaciones;
+    $scope.planillaC.w16=$scope.planillaC.w14+$scope.planillaC.w15;
+    $scope.planillaC.x16=$scope.planillaC.x14+$scope.planillaC.x15;
+    $scope.planillaC.z16=$scope.planillaC.z14+$scope.planillaC.z15;
+    $scope.planillaC.c17=$scope.planillaC.d16*$scope.planillaC.c14;
+    $scope.planillaC.d17=436;
+    $scope.planillaC.c18=datos.impuestoZnAlicuota*100;
+    $scope.planillaC.y18=$scope.planillaC.w14*$scope.planillaC.x8;
+    $scope.planillaC.z18=$scope.planillaC.x14-$scope.planillaC.y18;
+    $scope.planillaC.c19=$scope.planillaC.c17*$scope.planillaC.c18/100;
+    $scope.planillaC.y19=$scope.planillaC.w15*$scope.planillaC.x8;
+    $scope.planillaC.z19=$scope.planillaC.x15-$scope.planillaC.y19;
+    $scope.planillaC.u20=$scope.planillaC.d20/100;
+    $scope.planillaC.z20=$scope.planillaC.z18+$scope.planillaC.z19;
+    $scope.planillaC.c24=$scope.planillaC.c23*datos.pesoHumedadFactores;
+    $scope.planillaC.d25=datos.baseAgCotizaciones;
+    $scope.planillaC.c26=$scope.planillaC.c24*$scope.planillaC.d25;
+    $scope.planillaC.c27=datos.impuestoAgAlicuota*100;
+    $scope.planillaC.c28=$scope.planillaC.c26*$scope.planillaC.c27/100;
+    $scope.planillaC.c30=$scope.planillaC.c28+$scope.planillaC.c29;
+    $scope.planillaC.c31=$scope.planillaC.c30*$scope.planillaC.v6;
+    $scope.planillaC.c32=$scope.planillaC.c26+$scope.planillaC.c17;
   });
 });
