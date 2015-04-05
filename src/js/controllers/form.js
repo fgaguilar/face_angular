f6ac51378bd06442c6ec3ceaaf088974fada10d4
Fidel Gustavo Aguilar Flores <fgaguilar@yahoo.com>
2015-04-05 18:33:24 -0400

    ANB Se quito http://localhost para que llame el servicio de otros equipos diferentes al localhost

--- src/js/controllers/form.js
+++ src/js/controllers/form.js
@@ -2,7 +2,7 @@
 
 app.factory('Plan', ['$resource',
   function($resource){
-    return $resource('http://localhost/face_laravel/public/api/planillas/:planillaId', {}, {
+    return $resource('/face_laravel/public/api/planillas/:planillaId', {}, {
       'get': {method:'GET', params:{planillaId:'@planillaId'}, isArray:false},
       'save': {method:'POST'},
       'query': {method:'GET', isArray:true},
@@ -14,7 +14,7 @@ app.factory('Plan', ['$resource',
 
 app.factory('Dosificacion', ['$resource',
   function($resource){
-    return $resource('http://localhost/face_laravel/public/api/dosificaciones/:dosificacionId', {}, {
+    return $resource('/face_laravel/public/api/dosificaciones/:dosificacionId', {}, {
       'get': {method:'GET', params:{dosificacionId:'@dosificacionId'}, isArray:false},
       'save': {method:'POST'},
       'query': {method:'GET', isArray:true},
@@ -26,7 +26,7 @@ app.factory('Dosificacion', ['$resource',
 
 app.factory('Factura', ['$resource',
   function($resource){
-    return $resource('http://localhost/face_laravel/public/api/facturas/:facturaId', {}, {
+    return $resource('/face_laravel/public/api/facturas/:facturaId', {}, {
       'get': {method:'GET', params:{facturaId:'@facturaId'}, isArray:false},
       'save': {method:'POST'},
       'query': {method:'GET', isArray:true},
@@ -38,21 +38,21 @@ app.factory('Factura', ['$resource',
 
 app.factory('Facturac', ['$resource',
   function($resource){
-    return $resource('http://localhost/face_laravel/public/api/factura/:planillaId', {}, {
+    return $resource('/face_laravel/public/api/factura/:planillaId', {}, {
       'get': {method:'GET', params:{planillaId:'@planillaId'}, isArray:true}
     });
   }]);
 
 app.factory('PlanTipo', ['$resource',
   function($resource){
-    return $resource('http://localhost/face_laravel/public/api/planillasTipo/:tipoPlanilla', {}, {
+    return $resource('/face_laravel/public/api/planillasTipo/:tipoPlanilla', {}, {
       'get': {method:'GET', params:{tipoPlanilla:'@tipoPlanilla'}, isArray:true}
     });
   }]);
 
 app.factory('Parametro', ['$resource',
   function($resource){
-    return $resource('http://localhost/face_laravel/public/api/parametros/:parametroId', {}, {
+    return $resource('/face_laravel/public/api/parametros/:parametroId', {}, {
       'get': {method:'GET', params:{parametroId:'@parametroId'}, isArray:false},
       'save': {method:'POST'},
       'query': {method:'GET', isArray:true},
