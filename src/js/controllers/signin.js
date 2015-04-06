22e33d7cec9b660247c2d7e906be2208c8802a86
Fidel Gustavo Aguilar Flores <fgaguilar@yahoo.com>
2015-04-06 11:19:51 -0400

    ANB varios

--- src/js/controllers/signin.js
+++ src/js/controllers/signin.js
@@ -1,23 +1,43 @@
 'use strict';
-
+app.factory('Auth', ['$resource',
+  function($resource){
+    return $resource('http://localhost/face_laravel/public/service/authenticate/:planillaId', {}, {
+      'get': {method:'GET', params:{planillaId:'@planillaId'}, isArray:false},
+      'save': {method:'POST'},
+      'query': {method:'GET', isArray:true},
+      'update': {method:'PUT'},
+      'remove': {method:'DELETE'},
+      'delete': {method:'DELETE'}
+    });
+  }]);
 /* Controllers */
   // signin controller
-app.controller('SigninFormController', ['$scope', '$http', '$state', function($scope, $http, $state) {
+app.controller('SigninFormController', ['$scope', '$http', '$state', 'Auth', function($scope, $http, $state, Auth) {
     $scope.user = {};
     $scope.authError = null;
+
     $scope.login = function() {
-      $scope.authError = null;
-      // Try to login
-      $http.post('api/login', {email: $scope.user.email, password: $scope.user.password})
-      .then(function(response) {
-        if ( !response.data.user ) {
-          $scope.authError = 'Email or Password not right';
-        }else{
-          $state.go('app.dashboard-v1');
-        }
-      }, function(x) {
-        $scope.authError = 'Server Error';
+      Auth.save($scope.user, function() {
+        $timeout(function() {
+          $location.path('/');
+        });
       });
     };
+
+    //$scope.login = function() {
+      //$scope.authError = null;
+
+      // Try to login
+      // $http.post('api/login', {email: $scope.user.email, password: $scope.user.password})
+      // .then(function(response) {
+      //   if ( !response.data.user ) {
+      //     $scope.authError = 'Email or Password not right';
+      //   }else{
+      //     $state.go('app.dashboard-v1');
+      //   }
+      // }, function(x) {
+      //   $scope.authError = 'Server Error';
+      // });
+    //};
   }])
 ;
