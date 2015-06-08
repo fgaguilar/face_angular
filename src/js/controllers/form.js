5dc61c2403243131610f196bbdeaf4c3caa320c9
Fidel Gustavo Aguilar Flores <fgaguilar@yahoo.com>
2015-06-08 08:52:30 -0400

    ANB Adicion del fecha a paramteros

--- src/js/controllers/form.js
+++ src/js/controllers/form.js
@@ -1,67 +1,5 @@
 'use strict';
 
-/*app.factory('Plan', ['$resource',
-  function($resource){
-    return $resource('/face_laravel/public/api/planillas/:planillaId', {}, {
-      'get': {method:'GET', params:{planillaId:'@planillaId'}, isArray:false},
-      'save': {method:'POST'},
-      'query': {method:'GET', isArray:true},
-      'update': {method:'PUT'},
-      'remove': {method:'DELETE'},
-      'delete': {method:'DELETE'}
-    });
-  }]);
-
-app.factory('Dosificacion', ['$resource',
-  function($resource){
-    return $resource('/face_laravel/public/api/dosificaciones/:dosificacionId', {}, {
-      'get': {method:'GET', params:{dosificacionId:'@dosificacionId'}, isArray:false},
-      'save': {method:'POST'},
-      'query': {method:'GET', isArray:true},
-      'update': {method:'PUT'},
-      'remove': {method:'DELETE'},
-      'delete': {method:'DELETE'}
-    });
-  }]);
-
-app.factory('Factura', ['$resource',
-  function($resource){
-    return $resource('/face_laravel/public/api/facturas/:facturaId', {}, {
-      'get': {method:'GET', params:{facturaId:'@facturaId'}, isArray:false},
-      'save': {method:'POST'},
-      'query': {method:'GET', isArray:true},
-      'update': {method:'PUT'},
-      'remove': {method:'DELETE'},
-      'delete': {method:'DELETE'}
-    });
-  }]);
-
-app.factory('Facturac', ['$resource',
-  function($resource){
-    return $resource('/face_laravel/public/api/factura/:planillaId', {}, {
-      'get': {method:'GET', params:{planillaId:'@planillaId'}, isArray:true}
-    });
-  }]);
-
-app.factory('PlanTipo', ['$resource',
-  function($resource){
-    return $resource('/face_laravel/public/api/planillasTipo/:tipoPlanilla', {}, {
-      'get': {method:'GET', params:{tipoPlanilla:'@tipoPlanilla'}, isArray:true}
-    });
-  }]);
-
-app.factory('Parametro', ['$resource',
-  function($resource){
-    return $resource('/face_laravel/public/api/parametros/:parametroId', {}, {
-      'get': {method:'GET', params:{parametroId:'@parametroId'}, isArray:false},
-      'save': {method:'POST'},
-      'query': {method:'GET', isArray:true},
-      'update': {method:'PUT'},
-      'remove': {method:'DELETE'},
-      'delete': {method:'DELETE'}
-    });
-  }]);*/
-
 app.run(function($locale){
     $locale.NUMBER_FORMATS.GROUP_SEP = ",";
 });
@@ -718,6 +656,7 @@ app.controller('ParametrosCtrl',function ($scope,$location,$timeout,$stateParams
   $scope.parametro={};
   $scope.planilla2=Parametro.get({'parametroId': parametro}, function(datos){
     $scope.parametro=datos;
+    $scope.fecha=datos.updated_at;
     $scope.grabar = function() {
       console.log("Ingreso a Actualizar");
       Parametro.update({parametroId: $scope.parametro.id}, $scope.parametro, function() {
